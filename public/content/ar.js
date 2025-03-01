8fce72371176098b3d4c8b6cb2f4f9c7e416b72b
Desire32 <krutue@mail.ru>
2025-03-02 00:59:52 +0200

    added menu

---
+++ public/content/ar.js
@@ -0,0 +1,21 @@
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.style.display = (menu.style.display === 'none') ? 'block' : 'none';
+  }
+  function goBack() {
+    document.getElementById('description-popup').style.display = 'none';
+  }
+  function proceed() {
+    alert('Proceeding with the selected option!');
+    document.getElementById('description-popup').style.display = 'none';
+  }
+
+  function switchScene(sceneId) {
+    const scenes = ['scene-gps-text', 'scene-marker', 'scene-map'];
+    scenes.forEach((id) => {
+      const scene = document.getElementById(id);
+      if (scene) scene.style.display = 'none';
+    });
+    const scene = document.getElementById(sceneId);
+    if (scene) scene.style.display = 'block';
+  }
