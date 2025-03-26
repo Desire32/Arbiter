9e9e1d3a2f8bb446c00193c2ad4058ada3f01ee6
Desire32 <krutue@mail.ru>
2025-03-27 01:24:38 +0200

    vercel path fix

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,19 @@ export default function Home() {
     <div className={styles.container}>
       {/* First Section - Arbiter */}
       <section className={styles["section"]}>
-      <div>
-        <div className={styles["background-image"]}>
-          <Image
-            src="/public/static/images/uni_1.jpg"
-            alt="background image"
-            fill
-            style={{ objectFit: "cover" }}
-          />
+        <div>
+          <div className={styles["background-image"]}>
+            <Image
+              src="/static/images/uni_1.jpg" // Исправлено: убрали /public
+              alt="background image"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
         </div>
-      </div>
         <div className={styles["header-container"]}>
           <Image
-            src="/public/static/images/app_logo-removebg-preview.png"
+            src="/static/images/app_logo-removebg-preview.png" // Исправлено: убрали /public
             alt="logo"
             width={150}
             height={150}
@@ -39,25 +39,25 @@ export default function Home() {
 
       {/* Second Section - Geolocation */}
       <section className={styles["section"]}>
-      <div>
-        <div className={styles["background-image"]}>
-          <Image
-            src="/public/static/images/uni_2.jpg"
-            alt="background image"
-            fill
-            style={{ objectFit: "cover" }}
-          />
+        <div>
+          <div className={styles["background-image"]}>
+            <Image
+              src="/static/images/uni_2.jpg" // Исправлено: убрали /public
+              alt="background image"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
         </div>
-      </div>
         <div className={styles["description-container"]}>
           <p className={styles["logo-text"]}>What can it do?</p>
           <p className={styles["description-text"]}>
-          Arbiter is your new AR assistant for university. It makes life on campus easier and more fun. With augmented reality, you can scan markers and instantly get important info — whether it is your schedule, the location of classrooms, or even 3D models. Lost? No problem, just open the app and it will show you the way to your destination. It is simple, intuitive, and hassle-free. Get the most out of your studies and campus navigation, staying one step ahead!
+            Arbiter is your new AR assistant for university. It makes life on campus easier and more fun. With augmented reality, you can scan markers and instantly get important info — whether it is your schedule, the location of classrooms, or even 3D models. Lost? No problem, just open the app and it will show you the way to your destination. It is simple, intuitive, and hassle-free. Get the most out of your studies and campus navigation, staying one step ahead!
           </p>
         </div>
         <div className={styles["header-container"]}>
           <Image
-            src="/public/static/images/robot_no_background.png"
+            src="/static/images/robot_no_background.png" // Исправлено: убрали /public
             alt="logo"
             width={400}
             height={400}
@@ -70,20 +70,21 @@ export default function Home() {
 
       {/* Third Section - Markers */}
       <section className={styles["section"]}>
-      <div>
-        <div className={styles["background-image"]}>
-          <Image
-            src="/public/static/images/students.jpg"
-            alt="background image"
-            fill
-            style={{ objectFit: "cover" }}
-          />
+        <div>
+          <div className={styles["background-image"]}>
+            <Image
+              src="/static/images/students.jpg" // Исправлено: убрали /public
+              alt="background image"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
         </div>
-      </div>
         <div className={styles["description-container"]}>
           <p className={styles["logo-text"]}>What was the purpose of Arbiter?</p>
           <p className={styles["description-text"]}>
-          Arbiter was created to help people with disabilities navigate the campus and access important information more easily. The goal was to make the university more accessible and user-friendly, so that everyone could find their way around and get the information they need with minimal effort. With the app is augmented reality features, people with disabilities can quickly and easily get assistance with navigation, as well as access schedules, 3D models, and other resources. All you need to do is point the camera, and the information will be right there on your screen.</p>
+            Arbiter was created to help people with disabilities navigate the campus and access important information more easily. The goal was to make the university more accessible and user-friendly, so that everyone could find their way around and get the information they need with minimal effort. With the app is augmented reality features, people with disabilities can quickly and easily get assistance with navigation, as well as access schedules, 3D models, and other resources. All you need to do is point the camera, and the information will be right there on your screen.
+          </p>
         </div>
         <div className={styles["button-container"]}>
           <Link href='/content/ar.html'>
@@ -94,27 +95,27 @@ export default function Home() {
         </div>
       </section>
       
-    {/* About Section */}
-    <div className={styles["about-section"]}>
-      <h2>About Us</h2>
-      <p>
-        We are the team behind Arbiter, working to bring you an intuitive and powerful solution for your needs. Our goal is to make your experience seamless and enjoyable.
-      </p>
-      <div className={styles["about-container"]}>
-        <div className={styles["about-item"]}>
-          <h4>Nikita Markov</h4>
-          <p>Team Lead</p>
-        </div>
-        <div className={styles["about-item"]}>
-          <h4>Maxim Zharkov</h4>
-          <p>UI/UX Designer</p>
-        </div>
-        <div className={styles["about-item"]}>
-          <h4>Sergey Parenkov</h4>
-          <p>CI/CD Tester</p>
+      {/* About Section */}
+      <div className={styles["about-section"]}>
+        <h2>About Us</h2>
+        <p>
+          We are the team behind Arbiter, working to bring you an intuitive and powerful solution for your needs. Our goal is to make your experience seamless and enjoyable.
+        </p>
+        <div className={styles["about-container"]}>
+          <div className={styles["about-item"]}>
+            <h4>Nikita Markov</h4>
+            <p>Team Lead</p>
+          </div>
+          <div className={styles["about-item"]}>
+            <h4>Maxim Zharkov</h4>
+            <p>UI/UX Designer</p>
+          </div>
+          <div className={styles["about-item"]}>
+            <h4>Sergey Parenkov</h4>
+            <p>CI/CD Tester</p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 }
