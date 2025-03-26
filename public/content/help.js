7a20e73aeaa437aedbf95548a1a370cc11c8266f
Desire32 <krutue@mail.ru>
2025-03-26 16:59:43 +0200

    maps to screen

--- public/content/help.js
+++
@@ -1,9 +0,0 @@
-function toggleHelpMenu() {
-    const helpContainer = document.getElementById('help-container');
-    helpContainer.style.display = (helpContainer.style.display === 'block') ? 'none' : 'block';
-  }
-
-  function closeHelpMenu() {
-    const helpContainer = document.getElementById('help-container');
-    helpContainer.style.display = 'none';
-}
